dad0f985eae00fb97d42249957e262885ca1a580
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-11-13 16:53:22 +0100

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Rediriger vers la page d'accueil après la connexion
+  def after_sign_in_path_for(resource)
+    home_path  # Remplace "home_path" par le chemin de ta page d'accueil
+  end
 end
