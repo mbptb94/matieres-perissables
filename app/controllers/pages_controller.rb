fc76f51af46e073bf67e6fb11fd59d531fca729d
Yoann Tourtellier <tourtellier.yoann@gmail.com>
2024-11-13 16:19:15 +0100

    menu ok

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def asics
+  end
+
+  def cablage
+  end
+
+  def da
+  end
+
+  def coating
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/asics'
+  get 'pages/cablage'
+  get 'pages/da'
+  get 'pages/coating'
+  get 'asics', to: 'pages#asics'
+  get 'cablage', to: 'pages#cablage'
+  get 'da', to: 'pages#da'
+  get 'coating', to: 'pages#coating'
+  
+  get 'home/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -6,5 +17,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+root "home#index"
+
 end
